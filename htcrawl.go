03e70623274dfae7110051470b443f3b421c43af
Anup Ramachandran <anup.ramachandran@campus.tu-berlin.de>
2020-11-16 03:04:32 +0100

    GET links

--- htcrawl.go
+++ htcrawl.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"text/template"
 
 	"golang.org/x/net/html"
 )
@@ -27,112 +28,234 @@ func headingCount(heading []string) map[string]int {
 	return level
 }
 
-func main() { //main function
+func fetchInfo(w http.ResponseWriter, r *http.Request) {
 
-	baseURL := "https://www.w3.org/TR/xhtml1/"
-	var heading []string
-	var internal, external, inacessible int
-	var form bool
+	fmt.Println("method:", r.Method) //get request method
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("info.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		baseURL := strings.Join(r.Form["url"], " ")
 
-	//skip https
-	config := &tls.Config{
-		InsecureSkipVerify: true,
-	}
+		//baseURL := "https://www.w3.org/TR/xhtml1/"
+		var heading []string
+		var internal, external, inacessible int
+		var form bool
+		var version, title string
 
-	transport := &http.Transport{
-		TLSClientConfig: config,
-	}
+		//skip https
+		config := &tls.Config{
+			InsecureSkipVerify: true,
+		}
 
-	netClient := &http.Client{
-		Transport: transport,
-	}
+		transport := &http.Transport{
+			TLSClientConfig: config,
+		}
 
-	response, err := netClient.Get(baseURL)
+		netClient := &http.Client{
+			Transport: transport,
+		}
 
-	checkErr(err)
+		response, err := netClient.Get(baseURL)
 
-	z := html.NewTokenizer(response.Body)
-loop:
-	for {
-		tt := z.Next()
-		t := z.Token()
+		checkErr(err)
 
-		switch {
-		case tt == html.ErrorToken:
-			break loop
+		z := html.NewTokenizer(response.Body)
+	loop:
+		for {
+			tt := z.Next()
+			t := z.Token()
 
-		case tt == html.DoctypeToken:
 			switch {
-			case t.Data == "html":
-				fmt.Println("HTML Version: HTML5")
-			case t.Data == "HTML":
-				fmt.Println("HTML Version: HTML5 and beyond")
-			case strings.Contains(t.Data, "HTML 4.01"):
-				fmt.Println("HTML Version: HTML 4.01")
-			case strings.Contains(t.Data, "HTML 3.2"):
-				fmt.Println("HTML Version: HTML 3.2")
-			case strings.Contains(t.Data, "HTML 2.0"):
-				fmt.Println("HTML Version: HTML 2.0")
-			case strings.Contains(t.Data, "XHTML 1.0"):
-				fmt.Println("HTML Version: XHTML 1.0")
-			case strings.Contains(t.Data, "XHTML 1.1"):
-				fmt.Println("HTML Version: XHTML 1.1")
-			}
+			case tt == html.ErrorToken:
+				break loop
 
-		case tt == html.StartTagToken:
-			isTitle := t.Data == "title"
-			if isTitle {
-				z.Next()
-				fmt.Println("Title:", string(z.Text()))
-			}
+			case tt == html.DoctypeToken:
+				switch {
+				case t.Data == "html":
+					version = "HTML5"
+				case t.Data == "HTML":
+					version = "HTML5 and beyond"
+				case strings.Contains(t.Data, "HTML 4.01"):
+					version = "HTML 4.01"
+				case strings.Contains(t.Data, "HTML 3.2"):
+					version = "HTML 3.2"
+				case strings.Contains(t.Data, "HTML 2.0"):
+					version = "HTML 2.0"
+				case strings.Contains(t.Data, "XHTML 1.0"):
+					version = "XHTML 1.0"
+				case strings.Contains(t.Data, "XHTML 1.1"):
+					version = "XHTML 1.1"
+				}
 
-			headingPattern, _ := regexp.Compile(`h[0-9]`)
-			isHeading := headingPattern.MatchString(t.Data)
-			if isHeading {
-				heading = append(heading, t.Data)
-			}
+			case tt == html.StartTagToken:
+				isTitle := t.Data == "title"
+				if isTitle {
+					z.Next()
+					title = string(z.Text())
+				}
 
-			isAnchor := t.Data == "a"
-			if isAnchor {
+				headingPattern, _ := regexp.Compile(`h[0-9]`)
+				isHeading := headingPattern.MatchString(t.Data)
+				if isHeading {
+					heading = append(heading, t.Data)
+				}
 
-				linkPattern, _ := regexp.Compile("http|https")
-				var link string
-				for _, a := range t.Attr {
-					if a.Key == "href" {
-						isExternal := linkPattern.MatchString(a.Val)
-						if isExternal {
-							external++
-							link = a.Val
-						} else {
-							internal++
-							link = fmt.Sprintf("%s%s", baseURL, a.Val)
+				isAnchor := t.Data == "a"
+				if isAnchor {
+
+					linkPattern, _ := regexp.Compile("http|https")
+					var link string
+					for _, a := range t.Attr {
+						if a.Key == "href" {
+							isExternal := linkPattern.MatchString(a.Val)
+							if isExternal {
+								external++
+								link = a.Val
+							} else {
+								internal++
+								link = fmt.Sprintf("%s%s", baseURL, a.Val)
+							}
+							_, err := url.ParseRequestURI(link)
+							if err != nil {
+								inacessible++
+							}
+							//fmt.Println("Found href:", link)
+							break
 						}
-						_, err := url.ParseRequestURI(link)
-						if err != nil {
-							inacessible++
-						}
-						//fmt.Println("Found href:", link)
-						break
 					}
 				}
-			}
 
-			isForm := t.Data == "form"
-			if isForm {
-				form = true
+				isForm := t.Data == "form"
+				if isForm {
+					form = true
+				}
 			}
 		}
-	}
 
-	for headingLevel, count := range headingCount(heading) {
-		fmt.Println(headingLevel, ":", count)
+		fmt.Fprintf(w, "Title: %s\n", title)
+		fmt.Fprintf(w, "HTML Version: %s\n", version)
+		for headingLevel, count := range headingCount(heading) {
+			fmt.Fprintf(w, "%s:%d\n", headingLevel, count)
+		}
+		fmt.Fprintf(w, "Internal Links: %d , External Links: %d \n", internal, external)
+		fmt.Fprintf(w, "Inaccesible Links: %d\n", inacessible)
+		fmt.Fprintf(w, "Login Form Found: %t\n", form)
 	}
-	fmt.Println("Internal Links:", internal, " External Links:", external)
-	fmt.Println("Inaccesible Links:", inacessible)
-	fmt.Println("Form Found: ", form)
+}
+
+func main() { //main function
+
+	http.HandleFunc("/", fetchInfo)
+	http.ListenAndServe(":8000", nil)
+
+	/* baseURL := "https://www.w3.org/TR/xhtml1/"
+		var heading []string
+		var internal, external, inacessible int
+		var form bool
+
+		//skip https
+		config := &tls.Config{
+			InsecureSkipVerify: true,
+		}
+
+		transport := &http.Transport{
+			TLSClientConfig: config,
+		}
+
+		netClient := &http.Client{
+			Transport: transport,
+		}
+
+		response, err := netClient.Get(baseURL)
+
+		checkErr(err)
+
+		z := html.NewTokenizer(response.Body)
+	loop:
+		for {
+			tt := z.Next()
+			t := z.Token()
+
+			switch {
+			case tt == html.ErrorToken:
+				break loop
+
+			case tt == html.DoctypeToken:
+				switch {
+				case t.Data == "html":
+					fmt.Println("HTML Version: HTML5")
+				case t.Data == "HTML":
+					fmt.Println("HTML Version: HTML5 and beyond")
+				case strings.Contains(t.Data, "HTML 4.01"):
+					fmt.Println("HTML Version: HTML 4.01")
+				case strings.Contains(t.Data, "HTML 3.2"):
+					fmt.Println("HTML Version: HTML 3.2")
+				case strings.Contains(t.Data, "HTML 2.0"):
+					fmt.Println("HTML Version: HTML 2.0")
+				case strings.Contains(t.Data, "XHTML 1.0"):
+					fmt.Println("HTML Version: XHTML 1.0")
+				case strings.Contains(t.Data, "XHTML 1.1"):
+					fmt.Println("HTML Version: XHTML 1.1")
+				}
+
+			case tt == html.StartTagToken:
+				isTitle := t.Data == "title"
+				if isTitle {
+					z.Next()
+					fmt.Println("Title:", string(z.Text()))
+				}
+
+				headingPattern, _ := regexp.Compile(`h[0-9]`)
+				isHeading := headingPattern.MatchString(t.Data)
+				if isHeading {
+					heading = append(heading, t.Data)
+				}
+
+				isAnchor := t.Data == "a"
+				if isAnchor {
+
+					linkPattern, _ := regexp.Compile("http|https")
+					var link string
+					for _, a := range t.Attr {
+						if a.Key == "href" {
+							isExternal := linkPattern.MatchString(a.Val)
+							if isExternal {
+								external++
+								link = a.Val
+							} else {
+								internal++
+								link = fmt.Sprintf("%s%s", baseURL, a.Val)
+							}
+							_, err := url.ParseRequestURI(link)
+							if err != nil {
+								inacessible++
+							}
+							//fmt.Println("Found href:", link)
+							break
+						}
+					}
+				}
+
+				isForm := t.Data == "form"
+				if isForm {
+					form = true
+				}
+			}
+		}
+
+		for headingLevel, count := range headingCount(heading) {
+			fmt.Println(headingLevel, ":", count)
+		}
+		fmt.Println("Internal Links:", internal, " External Links:", external)
+		fmt.Println("Inaccesible Links:", inacessible)
+		fmt.Println("Login Form Found: ", form) */
 	//fmt.Println(heading)
 	//response.Body.Close()
 
+	/* http.HandleFunc("/", fetchInfo)
+	http.ListenAndServe(":8000", nil) */
 	/* a_page = get_url(a_page, "http://google.com")
 	http.HandleFunc("/", index_handler)//function based on the path "/"
 	http.ListenAndServe(":8000", nil)// (port,server which is nill) */
